631d41fe0ace9d200aa30e0a25f932cf30d088df
Blaine Bublitz <phate666x@gmail.com>
2012-07-02 15:22:42 -0700

    amd-ify

--- scripts/mwe/InputManager.js
+++ scripts/mwe/InputManager.js
@@ -1,5 +1,5 @@
 /**
- 
+
  Copyright 2011 Luis Montes
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,141 +16,103 @@ limitations under the License.
 
 **/
 
-dojo.provide("mwe.InputManager");
-
 /*********************** mwe.InputManager ********************************************/
-dojo.declare("mwe.InputManager",null,{
-	
-	keyActions: [],
-	mouseAction: null,
-	touchAction: null,
-	
-	canvas: null,
+define(['./GameAction', 'dojo/_base/declare', 'dojo/_base/connect', 'dojo/dom-geometry'], function(GameAction, declare, connect, domGeom){
 
+  return declare(null, {
+    keyActions: [],
+    mouseAction: null,
+    touchAction: null,
+    canvas: null,
     constructor: function(args){
-		dojo.safeMixin(this, args);
-		dojo.connect(document,"onkeydown",this,this.keyPressed);
-		dojo.connect(document,"onkeyup",this,this.keyReleased);
-		dojo.connect(this.canvas,"onmousedown",this,this.mouseDown);
-		dojo.connect(document,"onmouseup",this,this.mouseUp);
-		dojo.connect(this.canvas,"onmousemove",this,this.mouseMove);
-		
-		dojo.connect(document,'ontouchend',this,this.touchEnd);
-		dojo.connect(this.canvas,'ontouchstart',this.touchStart);
-		dojo.connect(this.canvas,'ontouchmove',this.touchMove);
-	},
+      declare.safeMixin(this, args);
+      // TODO: switch to dojo/on
+      connect.connect(document, 'onkeydown', this, this.keyPressed);
+      connect.connect(document, 'onkeyup', this, this.keyReleased);
+      connect.connect(this.canvas, 'onmousedown', this, this.mouseDown);
+      connect.connect(document, 'onmouseup', this, this.mouseUp);
+      connect.connect(this.canvas, 'onmousemove', this, this.mouseMove);
 
-
+      connect.connect(document, 'ontouchend', this, this.touchEnd);
+      connect.connect(this.canvas, 'ontouchstart', this, this.touchStart);
+      connect.connect(this.canvas, 'ontouchmove', this, this.touchMove);
+    },
     /**
-    Maps a GameAction to a specific key. The key codes are
-    defined in java.awt.KeyEvent. If the key already has
-    a GameAction mapped to it, the new GameAction overwrites
-    it.
-	*/
-	mapToKey: function(gameAction, keyCode) {
-    	if(!this.keyActions){
-    		this.keyActions = [];
-    	}
-	    this.keyActions[keyCode] = gameAction;
-	},
-	
-	addKeyAction: function(keyCode,initialPressOnly){
-		var ga = new mwe.GameAction();
-		if(initialPressOnly){
-			ga.behavior = ga.statics.DETECT_INITAL_PRESS_ONLY;
-		}		
-		this.mapToKey(ga,keyCode);	
-	},
-	
-	setMouseAction: function(gameAction){
-		this.mouseAction = gameAction;		
-	},
-	
-	setTouchAction: function(gameAction){
-		this.touchAction = gameAction;		
-	},
-	
-	
-	mouseUp: function(e){
-		
-		
-	},
-	
-	mouseDown: function(e){
-		
-		
-	},
-	
-	mouseMove: function(e){
-		
-	},
-	
-	touchStart: function(e){
-		
-		
-	},
-	
-	touchEnd: function(e){
-		
-		
-	},
-	
-	touchMove: function(e){
-		
-	},
-	
-	
-	getKeyAction: function(e) {
-	    
-	    if (this.keyActions.length) {
-	        return this.keyActions[e.keyCode];
-	    }
-	    else {
-	        return null;
-	    }
-	},
-	
-	keyPressed : function(e) {
-	    var gameAction = this.getKeyAction(e);
-	    if (gameAction && (!gameAction.isPressed())) {
-	    	gameAction.press();
-	    }
+      Maps a GameAction to a specific key. The key codes are defined in java.awt.KeyEvent.
+      If the key already has a GameAction mapped to it, the new GameAction overwrites it.
+    */
+    mapToKey: function(gameAction, keyCode) {
+      if(!this.keyActions){
+        this.keyActions = [];
+      }
+      this.keyActions[keyCode] = gameAction;
+    },
+    addKeyAction: function(keyCode, initialPressOnly){
+      // TODO: Remove dependency on GameAction
+      var ga = new GameAction();
+      if(initialPressOnly){
+        ga.behavior = ga.statics.DETECT_INITAL_PRESS_ONLY;
+      }
+      this.mapToKey(ga,keyCode);
+    },
+    setMouseAction: function(gameAction){
+      this.mouseAction = gameAction;
+    },
+    setTouchAction: function(gameAction){
+      this.touchAction = gameAction;
+    },
+    mouseUp: function(e) {
+      // this.mouseAction.release();
+    },
+    mouseDown: function(e){
+      // this.mouseAction.press();
+    },
+    mouseMove: function(e){
+      // this.mouseAction.move(e.clientX, e.clientY);
+    },
+    touchStart: function(e){
+      // this.touchAction.press();
+    },
+    touchEnd: function(e){
+      // this.touchAction.release();
+    },
+    touchMove: function(e){
+      // this.touchAction.move(e.clientX, e.clientY);
+    },
+    getKeyAction: function(e) {
+      if (this.keyActions.length) {
+        return this.keyActions[e.keyCode];
+      } else {
+        return null;
+      }
+    },
+    keyPressed: function(e) {
+      var gameAction = this.getKeyAction(e);
+      if (gameAction && !gameAction.isPressed()) {
+        gameAction.press();
+      }
+      // TODO: make sure the key isn't processed for anything else
+    },
+    keyReleased : function(e) {
+      var gameAction = this.getKeyAction(e);
+      if (gameAction !== null) {
+        gameAction.release();
+      }
+      // TODO: make sure the key isn't processed for anything else
+    },
+    keyTyped: function(e) {
+      // TODO: make sure the key isn't processed for anything else
+    },
+    /**
+      Get the mouse pointer location within the canvas' coordinates, not the page's
+    */
+    getMouseLoc: function(evt){
+      var coordsM = domGeom.getContentBox(this.canvas);
+      return {
+        x: Math.round(evt.clientX - coordsM.x),
+        y: Math.round(evt.clientY - coordsM.y)
+      };
+    }
+  });
 
-	    
-	    // make sure the key isn't processed for anything else
-	    // TODO
-	    //e.consume();
-	},
-	
-	
-	keyReleased : function(e) {
-	    var gameAction = this.getKeyAction(e);
-	    if (gameAction != null) {
-	        gameAction.release();
-	    }
-
-	    
-	    // make sure the key isn't processed for anything else
-	    // TODO
-	    //e.consume();
-	},
-	
-	
-	keyTyped: function(e) {
-	    // make sure the key isn't processed for anything else
-	    // TODO
-	    //e.consume();
-	},
-	
-	/**
-	 * Get the mouse pointer location within the canvas' coordinates, not the page's
-	 */
-	getMouseLoc: function(evt){
-		  var coordsM = dojo.coords(this.canvas);
-		  return {x: Math.round(evt.clientX - coordsM.x), y: Math.round(evt.clientY - coordsM.y)};
-	}
-	
-	
 });
-
-
--- scripts/mwe/ResourceManager.js
+++ scripts/mwe/ResourceManager.js
@@ -1,5 +1,5 @@
 /**
- 
+
  Copyright 2011 Luis Montes
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,82 +16,68 @@ limitations under the License.
 
 **/
 
-dojo.provide("mwe.ResourceManager");
-
-
 /*********************** mwe.ResourceManager ********************************************/
+define(['dojo/_base/declare'], function(declare){
 
-dojo.declare("mwe.ResourceManager",null,{
-
-	
-	imageCount: 0,
-	loadedImages: 0,
-	allLoaded: false,
-	
-	imageDir : 'images/',
-	imgList: [],
-
-	constructor: function(args){
-		dojo.safeMixin(this, args);
-
-	
-	},
-
+  return declare(null, {
+    imageCount: 0,
+    loadedImages: 0,
+    allLoaded: false,
+    imageDir: 'images/',
+    imgList: [],
+    constructor: function(args){
+      declare.safeMixin(this, args);
+    },
     /**
-    Gets an image.
-	*/
-	loadImage: function(filename,width, height) {
+      Gets an image.
+    */
+    loadImage: function(filename, width, height){
+      //if we already have the image, just return it
+      for(var i = 0; i < this.imgList.length; i++){
+        if(this.imgList[i].name == filename){
+          return this.imgList[i].img;
+        }
+      }
 
+      this.allLoaded = false;
 
-		//if we already have the image, just return it
-		for(i=0;i<this.imgList.length;i++){
-			if(this.imgList[i].name == filename ){
-				return this.imgList[i].img;
-			}
-		}
-
-		this.allLoaded = false;
-		
-	    var img = new Image();
-
-	    if(this.imageDir){
-	    	filename = this.imageDir + filename;
-	    }
-	    img.src = filename;
-
-	    this.imgList.push({name:filename,img:img});
-	    
-	    return img;
-	},
-
-	resourcesReady: function(){
-		if(this.allLoaded){
-			return true;
-		}else{
-			for(i=0; i<this.imgList.length; i++){
-				if(!this.imgList[i].img.complete){
-					return false;
-				}
-			}
-			this.allLoaded = true;
-			return true;
-		}
-	},
-	
-	getPercentComplete: function(){
-		var numComplete = 0.0;
-		for(i=0; i<this.imgList.length; i++){
-			if(this.imgList[i].img.complete){
-				numComplete = numComplete + 1.0;
-			}
-		}
-		if(this.imgList.length === 0){
-			return 0;
-		}else{
-			return Math.round((numComplete/this.imgList.length) * 100.0);
-		}
-		
-	
-	}
+      var img = new Image();
+      if(this.imageDir){
+        filename = this.imageDir + filename;
+      }
+      img.src = filename;
+      this.imgList.push({
+        name: filename,
+        img: img
+      });
+      return img;
+    },
+    resourcesReady: function(){
+      if(this.allLoaded){
+        return true;
+      }else{
+        for(var i = 0; i < this.imgList.length; i++){
+          if(!this.imgList[i].img.complete){
+            return false;
+          }
+        }
+        this.allLoaded = true;
+        return true;
+      }
+    },
+    getPercentComplete: function(){
+      var numComplete = 0.0;
+      for(var i = 0; i < this.imgList.length; i++){
+        if(this.imgList[i].img.complete){
+          numComplete = numComplete + 1.0;
+        }
+      }
+      if(this.imgList.length === 0){
+        return 0;
+      }else{
+        return Math.round((numComplete / this.imgList.length) * 100.0);
+      }
+    }
+  });
 
 });
